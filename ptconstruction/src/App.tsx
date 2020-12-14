c7a9508cbf2ebf82c437d7fc1aa2f38af566a11c
ashleyerffmeyer <ashley.erffmeyer@gmail.com>
2020-12-14 10:41:05 -0600

    initial typescript skeleton

---
+++ ptconstruction/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import NavBar from "./components/NavBar/NavBar";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <p>test</p>
+    </>
+  );
+}
+
+export default App;
